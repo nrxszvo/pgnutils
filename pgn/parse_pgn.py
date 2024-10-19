5751925f6d835bba660d2c32425551786840dfa0
Michael Horgan <michael.g.horgan@gmail.com>
2024-10-19 15:40:30 -0700

    add parallel chunk parser

--- pgn/parse_pgn.py
+++ pgn/parse_pgn.py
@@ -11,13 +11,165 @@ from lib import validate_game, parse_moves, PgnProcessor
 def get_eta(max_items, items_so_far, start):
     end = time.time()
     eta = datetime.timedelta(
-        seconds=(max_items - items_so_far) * (end - start) / items_so_far
+        seconds=max((max_items - items_so_far), 0) * (end - start) / items_so_far
     )
     hours = eta.seconds // 3600
     minutes = (eta.seconds % 3600) // 60
     seconds = eta.seconds % 60
     eta_str = f"{eta.days}:{hours}:{minutes:02}:{seconds:02}"
     return eta_str
+
+
+def process_chunk(pid, nproc, pgn_q, output_q, lock):
+    while True:
+        pgn = pgn_q.get()
+        if pgn == "DONE":
+            break
+        else:
+            nbytes = os.path.getsize(pgn)
+            chunk_size = int(nbytes / nproc)
+            byte_offset = pid * chunk_size
+            if pid == nproc - 1:
+                chunk_size = nbytes - byte_offset
+            bytes_processed = 0
+            gameid = 0
+
+            with open(pgn) as fin:
+                fin.seek(byte_offset)
+                processor = PgnProcessor()
+                for line in fin:
+                    bytes_processed += len(line)
+                    code = processor.process_line(line)
+                    if code == "COMPLETE":
+                        md = {
+                            "WhiteElo": processor.get_welo(),
+                            "BlackElo": processor.get_belo(),
+                            "gameid": gameid,
+                            "pid": pid,
+                        }
+                        gameid += 1
+                        mvids = parse_moves(processor.get_move_str())
+                        errs = validate_game(
+                            md["gameid"], processor.get_move_str(), mvids
+                        )
+                        if len(errs) == 0:
+                            output_q.put(("GAME", (pid, bytes_processed, md, mvids)))
+                        else:
+                            output_q.put(("ERROR", errs))
+                    if (
+                        code in ["COMPLETE", "INVALID"]
+                        and bytes_processed >= chunk_size
+                    ):
+                        break
+
+            output_q.put(("DONE", pid))
+
+
+def start_chunk_reader_procs(num_readers, pgn_q, output_q, lock):
+    procs = []
+    for pid in range(num_readers):
+        reader_p = Process(
+            target=process_chunk, args=(pid, num_readers, pgn_q, output_q, lock)
+        )
+        reader_p.daemon = True
+        reader_p.start()
+        procs.append(reader_p)
+    return procs
+
+
+class ParallelChunkParser:
+    def __init__(self, n_proc):
+        self.n_proc = n_proc
+        self.print_lock = Lock()
+        self.pgn_q = Queue()
+        self.output_q = Queue()
+        self.reader_ps = start_chunk_reader_procs(
+            n_proc, self.pgn_q, self.output_q, self.print_lock
+        )
+
+    def close(self):
+        for _ in range(self.n_proc):
+            self.pgn_q.put("DONE")
+        self.output_q.close()
+        self.pgn_q.close()
+        for rp in self.reader_ps:
+            rp.join()
+            rp.close()
+
+    def print_update(self):
+        for pid in range(self.n_proc):
+            eta_str = get_eta(self.chunk_size, self.pid_bp[pid], self.start)
+            self.pid_etas[pid] = eta_str
+        status_str = f"Parsed {self.total_games} games\n"
+        for pid, (eta, bp) in enumerate(zip(self.pid_etas, self.pid_bp)):
+            status_str += f"\t{pid}: {eta}\t{100*bp/self.chunksize:.1f}%\n"
+        self.print_lock.acquire()
+        try:
+            print(status_str, end="\r")
+        finally:
+            self.print_lock.release()
+
+    def parse(self, pgn):
+        self.start = time.time()
+        self.nbytes = os.path.getsize(pgn)
+        self.chunk_size = int(self.nbytes / self.n_proc)
+        self.pid_etas = ["tbd"] * self.n_proc
+        self.pid_bp = [0] * self.n_proc
+        self.total_games = 0
+
+        for _ in range(self.n_proc):
+            self.pgn_q.put(pgn)
+
+        n_finished = 0
+        all_pid_md = [[] for _ in range(self.n_proc)]
+        all_pid_mvids = [[] for _ in range(self.n_proc)]
+
+        while True:
+            code, data = self.output_q.get()
+            if code == "DONE":
+                pid = data
+                n_finished += 1
+                self.print_update()
+                if n_finished == self.n_proc:
+                    break
+            elif code == "ERROR":
+                self.print_lock.acquire()
+                try:
+                    print()
+                    for err in data:
+                        print(err)
+                finally:
+                    self.print_lock.release()
+            elif code == "GAME":
+                pid, bytesproc, md, mvids = data
+                self.pid_bp[pid] = bytesproc
+                nmoves = len(all_pid_mvids[pid])
+                md["start"] = nmoves
+                md["end"] = nmoves + len(mvids)
+                all_pid_md[pid].append(md)
+                all_pid_mvids[pid].extend(mvids)
+
+                self.total_games += 1
+                if self.total_games % 1000 == 0:
+                    self.print_update()
+            else:
+                raise Exception(f"invalid code: {code}")
+
+        games = [md for pid_md in all_pid_md for md in pid_md]
+        all_mvids = []
+        for pid_mvids in all_pid_mvids:
+            all_mvids.extend(pid_mvids)
+
+        end = time.time()
+        nsec = end - self.start
+        hr = int(nsec // 3600)
+        minute = int((nsec % 3600) // 60)
+        sec = int(nsec % 60)
+        print(
+            f"\nTotal time to parse pgn: {hr}:{minute:02d}:{sec:02d}                       "
+        )
+        all_md = {"shape": len(all_mvids), "games": games}
+        return all_md, all_mvids
 
 
 def load_games(pgn_q, games_q, num_readers, lock):
@@ -28,30 +180,26 @@ def load_games(pgn_q, games_q, num_readers, lock):
             gameid = 0
             with open(pgn) as fin:
                 processor = PgnProcessor()
-                while True:
-                    for line in fin:
-                        bytes_processed += len(line)
-                        code = processor.process_line(line)
-                        if code == "COMPLETE":
-                            md = {
-                                "WhiteElo": processor.get_welo(),
-                                "BlackElo": processor.get_belo(),
-                                "gameid": gameid,
-                            }
-                            gameid += 1
-                            games_q.put(
+                for line in fin:
+                    bytes_processed += len(line)
+                    code = processor.process_line(line)
+                    if code == "COMPLETE":
+                        md = {
+                            "WhiteElo": processor.get_welo(),
+                            "BlackElo": processor.get_belo(),
+                            "gameid": gameid,
+                        }
+                        gameid += 1
+                        games_q.put(
+                            (
+                                "GAME",
                                 (
-                                    "GAME",
-                                    (
-                                        bytes_processed,
-                                        md,
-                                        processor.get_move_str(),
-                                    ),
-                                )
+                                    bytes_processed,
+                                    md,
+                                    processor.get_move_str(),
+                                ),
                             )
-
-                    else:
-                        break  # EOF
+                        )
 
         if pgn == "DONE":
             for _ in range(num_readers):
@@ -153,7 +301,7 @@ class ParallelParser:
                 games.append(md)
                 all_mvids.extend(mvids)
 
-                if md["gameid"] % 1 == 0:
+                if md["gameid"] % 1000 == 0:
                     eta_str = get_eta(nbytes, bytesproc, start)
                     status_str = f"parsed {md['gameid']} games (eta: {eta_str})        "
                     self.print_lock.acquire()
@@ -263,10 +411,10 @@ def main():
     if args.serial:
         all_md, all_moves = main_serial(args.pgn)
     else:
-        parser = ParallelParser(args.n_proc)
+        parser = ParallelChunkParser(args.n_proc)
         all_md, all_moves = parser.parse(args.pgn)
+        parser.close()
 
-    print(f"\nNumber of games: {len(all_md['games'])}")
     if len(all_md["games"]) > 0:
         mdfile = f"{args.npy}_md.npy"
         mvfile = f"{args.npy}_moves.npy"
@@ -277,4 +425,5 @@ def main():
 
 
 if __name__ == "__main__":
+    __spec__ = None
     main()
