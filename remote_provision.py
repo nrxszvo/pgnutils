43067290539067843eb64b7a8d323c07d00fe544
Michael Horgan <michael.g.horgan@gmail.com>
2024-11-13 15:57:14 -0800

    update remote provision

--- remote_provision.py
+++ remote_provision.py
@@ -1,19 +1,28 @@
 import subprocess
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("remote", help="remote host login, e.g. ubuntu@192.168.0.0")
 parser.add_argument(
     "--tokenfile",
-    default="~/.githubaccesstoken",
+    default=os.path.expanduser("~/.githubaccesstoken"),
     help="text file containing github acccess token",
 )
+
+parser.add_argument("remote", help="remote host login, e.g. ubuntu@192.168.0.0")
+parser.add_argument("myname", help='for git config, e.g. "John Doe"')
+parser.add_argument("myemail", help="for git config, e.g. johndoe@gmail.com")
+
 args = parser.parse_args()
+
+myname = f'"{args.myname}"'
+myemail = f'"{args.myemail}"'
 
 with open(args.tokenfile) as f:
     token = f.readline()
 
 cmd = f"scp provision.sh {args.remote}:~"
 subprocess.call(cmd, shell=True)
-cmd = f'ssh {args.remote} "chmod 755 provision.sh; GHTOKEN={token} sh provision.sh"'
-subprocess.call(cmd, shell=True)
+cmd = f'ssh {args.remote} "chmod 755 provision.sh; GHTOKEN={token} MYNAME={myname} MYEMAIL={myemail} sh provision.sh"'
+print(cmd)
+# subprocess.call(cmd, shell=True)
