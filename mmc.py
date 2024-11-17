update for debug workflow; fix another off-by-one error in data loading; add count_valid metric to prediction script

mmc.py
@@ -47,6 +47,11 @@ class MimicChessCoreModule(L.LightningModule):
         logger = TensorBoardLogger(".", name="L", version=params.name)
         val_check_interval = min(params.val_check_steps, params.max_steps)
         self.lr_scheduler_params = params.lr_scheduler_params
+        if torch.cuda.is_available():
+            precision = "bf16-mixed" if torch.cuda.is_bf16_supported() else 16
+        else:
+            precision = 32
+
         self.trainer_kwargs = {
             "logger": logger,
             "max_steps": params.max_steps,
@@ -54,13 +59,14 @@ class MimicChessCoreModule(L.LightningModule):
             "check_val_every_n_epoch": None,
             "strategy": params.strategy,
             "devices": params.devices,
-            "precision": "bf16-mixed" if torch.cuda.is_bf16_supported() else 16,
+            "precision": precision,
+            "accelerator": "gpu" if torch.cuda.is_available() else "cpu",
         }
         self.callbacks = [
             TQDMProgressBar(),
             ModelCheckpoint(
                 dirpath=params.outdir,
-                filename=params.name + "-{val_loss:.2f}",
+                filename=params.name + "-{train_loss:.2f}",
                 every_n_train_steps=val_check_interval,
             ),
         ]
@@ -151,7 +157,7 @@ class MimicChessCoreModule(L.LightningModule):
 
     def training_step(self, batch, batch_idx):
         logits = self(batch["input"])
-        logits = logits[:, self.min_moves :].permute(0, 2, 1)
+        logits = logits[:, self.min_moves - 1 :].permute(0, 2, 1)
         tgt = batch["target"]
         loss = self.loss(logits, tgt, ignore_index=self.NOOP)
         self.log("train_loss", loss, prog_bar=True, sync_dist=True)
@@ -161,7 +167,7 @@ class MimicChessCoreModule(L.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         logits = self(batch["input"])
-        logits = logits[:, self.min_moves :].permute(0, 2, 1)
+        logits = logits[:, self.min_moves - 1 :].permute(0, 2, 1)
         tgt = batch["target"]
         valid_loss = self.loss(logits, tgt, ignore_index=self.NOOP)
 
@@ -192,13 +198,14 @@ class MimicChessCoreModule(L.LightningModule):
     def predict_step(self, batch, batch_idx):
         logits = self(batch["input"])
         tgt = batch["target"]
-        probs = torch.softmax(logits[:, self.min_moves :], dim=-1)
+        probs = torch.softmax(logits[:, self.min_moves - 1 :], dim=-1)
         tokens, probs = self.sample_top_n(probs, n=3)
-        return tokens, probs, tgt.unsqueeze(-1)
+        return tokens, probs, batch["opening"], tgt.unsqueeze(-1)
 
     def fit(self, datamodule):
         self.trainer.fit(self, datamodule=datamodule)
-        print(torch.cuda.memory_summary())
+        if torch.cuda.is_available():
+            print(torch.cuda.memory_summary())
 
     def predict(self, datamodule):
         tkargs = self.trainer_kwargs

mmcdataset.py
@@ -15,6 +15,7 @@ def init_worker(seed):
 def collate_fn(batch):
     maxinp = 0
     maxtgt = 0
+    openmoves = batch[0]["opening"].shape[0]
     for d in batch:
         inp = d["input"]
         tgt = d["target"]
@@ -24,6 +25,7 @@ def collate_fn(batch):
     bs = len(batch)
     inputs = torch.full((bs, maxinp), NOOP, dtype=torch.int32)
     targets = torch.full((bs, maxtgt), NOOP, dtype=torch.int64)
+    openings = torch.empty((bs, openmoves), dtype=torch.int64)
 
     for i, d in enumerate(batch):
         inp = d["input"]
@@ -32,8 +34,9 @@ def collate_fn(batch):
         nt = tgt.shape[0]
         inputs[i, :ni] = torch.from_numpy(inp)
         targets[i, :nt] = torch.from_numpy(tgt)
+        openings[i] = torch.from_numpy(d["opening"])
 
-    return {"input": inputs, "target": targets}
+    return {"input": inputs, "target": targets, "opening": openings}
 
 
 class MMCDataset(Dataset):
@@ -55,8 +58,10 @@ class MMCDataset(Dataset):
         inp = np.empty(n_inp, dtype="int32")
         inp[:] = self.mvids[gs : gs + n_inp]
 
-        tgt = np.empty(n_inp - self.min_moves, dtype="int64")
-        tgt[:] = self.mvids[gs + self.min_moves + 1 : gs + n_inp + 1]
+        opening = self.mvids[gs : gs + self.min_moves]
+
+        tgt = np.empty(n_inp + 1 - self.min_moves, dtype="int64")
+        tgt[:] = self.mvids[gs + self.min_moves : gs + n_inp + 1]
 
         if self.train_head:
             if code == 0:
@@ -67,6 +72,7 @@ class MMCDataset(Dataset):
         return {
             "input": inp,
             "target": tgt,
+            "opening": opening,
         }
 
 
@@ -161,6 +167,7 @@ class MMCDataModule(L.LightningDataModule):
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             worker_init_fn=init_worker,
+            persistent_workers=True,
         )
 
     def val_dataloader(self):
@@ -169,6 +176,7 @@ class MMCDataModule(L.LightningDataModule):
             collate_fn=collate_fn,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
+            persistent_workers=True,
         )
 
     def predict_dataloader(self):
@@ -177,6 +185,7 @@ class MMCDataModule(L.LightningDataModule):
             collate_fn=collate_fn,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
+            persistent_workers=True,
         )
 
     def test_dataloader(self):

predict.py
@@ -3,18 +3,17 @@ from pathlib import Path
 import os
 import sys
 import numpy as np
-from pgn.py.lib.reconstruct import reconstruct
+from pgn.py.lib.reconstruct import count_invalid
 
 import torch
 from fairscale.nn.model_parallel.initialize import (
     get_model_parallel_rank,
-    initialize_model_parallel,
-    model_parallel_is_initialized,
 )
 from config import get_config
 from mmc import MimicChessCoreModule, MMCModuleArgs, MimicChessHeadModule
 from mmcdataset import MMCDataModule, NOOP
 from model import ModelArgs
+from train import torch_init
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--cfg", required=True, help="yaml config file")
@@ -27,7 +26,6 @@ parser.add_argument(
 
 
 def main():
-    torch.set_float32_matmul_precision("high")
     args = parser.parse_args()
     cpfn = args.cp
     cfgfn = args.cfg
@@ -37,20 +35,9 @@ def main():
 
     cfgyml = get_config(cfgfn)
 
-    if not torch.distributed.is_initialized():
-        torch.distributed.init_process_group("nccl")
-
-    if not model_parallel_is_initialized():
-        model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
-        initialize_model_parallel(model_parallel_size)
-
-    local_rank = int(os.environ.get("LOCAL_RANK", 0))
-    torch.cuda.set_device(local_rank)
+    model_parallel_size = torch_init()
 
     torch.manual_seed(cfgyml.random_seed)
-
-    if local_rank > 0:
-        sys.stdout = open(os.devnull, "w")
 
     checkpoints = sorted(Path(args.cp).glob("*.ckpt"))
     assert len(checkpoints) > 0, f"no checkpoint files found in {args.cp}"
@@ -85,22 +72,25 @@ def main():
     outputs = mmc.predict(dm)
     ntotalpred = 0
     ntotalmatch = 0
-    nfail = 0
+    npass = 0
     ngm = 0
-    for tokens, probs, tgt in outputs:
+    nvalidmoves = 0
+    for tokens, probs, opening, tgt in outputs:
         matches = (tokens == tgt).sum(dim=-1, keepdim=True)
         matches[tgt == NOOP] = 0
         npred = (tgt != NOOP).sum()
         ntotalpred += npred
         ntotalmatch += matches.sum()
         ngm += tokens.shape[0]
-        for gm in tokens:
-            try:
-                reconstruct(gm[:, 0].cpu().numpy())
-            except Exception as e:
-                nfail += 1
+        for game in zip(tokens, opening, tgt):
+            pred, opn, tgt = game
+            nmoves, nfail = count_invalid(pred[:, 0], opn, tgt[:, 0])
+            nvalidmoves += nmoves - nfail
+            if nfail == 0:
+                npass += 1
 
-    print(f"{ngm-nfail} out of {ngm} are legal games")
+    print(f"{npass}/{ngm} are legal games")
+    print(f"Average number of legal moves per game: {nvalidmoves/ngm:.1f}")
     print(f"Top 3 accuracy: {100*ntotalmatch/ntotalpred:.2f}%")
 
 

train.py
@@ -41,7 +41,8 @@ parser.add_argument("--elo", default=None, help="elo tag for elo-specific head o
 
 
 def torch_init():
-    model_parallel_size = 1
+    model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
+    local_rank = int(os.environ.get("LOCAL_RANK", 0))
     torch.set_float32_matmul_precision("medium")
     if not torch.distributed.is_initialized():
         if torch.cuda.is_available():
@@ -50,15 +51,14 @@ def torch_init():
             torch.distributed.init_process_group("gloo")
 
     if not model_parallel_is_initialized():
-        model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
         initialize_model_parallel(model_parallel_size)
 
     if torch.cuda.is_available():
-        local_rank = int(os.environ.get("LOCAL_RANK", 0))
         torch.cuda.set_device(local_rank)
         if local_rank > 0:
             sys.stdout = open(os.devnull, "w")
     return model_parallel_size
+
 
 def main():
     args = parser.parse_args()