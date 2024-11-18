8eacc6ae5988138fec99dc0e18367da530963dbb
Michael <michaelghorgan@gmail.com>
2024-11-18 20:59:21 +0000

    update provision

--- provision.sh
+++ provision.sh
@@ -1,35 +1,36 @@
 CONDA_VER=latest
 OS_TYPE=x86_64
-CONDA_DIR=/home/ubuntu/miniconda
+CONDA_DIR=${HOME}/miniconda
 PY_VER=3.10
+cd ${HOME} 
 
-if [ ! -d "$CONDA_DIR" ]; then
+if [ ! -d "${CONDA_DIR}" ]; then
 	echo "removing local python site-packages..."
-	rm -rf /home/ubuntu/.local/lib/python3.10/site-packages
+	rm -rf ${HOME}/.local/lib/python3.10/site-packages
 	echo "installing conda..."
 	curl -LO "http://repo.continuum.io/miniconda/Miniconda3-${CONDA_VER}-Linux-${OS_TYPE}.sh"
-	bash Miniconda3-${CONDA_VER}-Linux-${OS_TYPE}.sh -p /home/ubuntu/miniconda -b
+	bash Miniconda3-${CONDA_VER}-Linux-${OS_TYPE}.sh -p ${HOME}/miniconda -b
 	rm Miniconda3-${CONDA_VER}-Linux-${OS_TYPE}.sh
 fi
 
 if ! command -v conda 2>&1 >/dev/null
 then
-	export PATH=/home/ubuntu/miniconda/bin:${PATH}
+	export PATH=${HOME}/miniconda/bin:${PATH}
 fi
 
 conda update -y conda
 conda init
 
-if [ ! -d "/home/ubuntu/git" ]; then
-	mkdir "/home/ubuntu/git"
+if [ ! -d "${HOME}/git" ]; then
+	mkdir ${HOME}/git
 fi
 
-if [ ! -d "/home/ubuntu/git/mimicChess" ]; then
+if [ ! -d "${HOME}/git/mimicChess" ]; then
 	cd git
 	git clone https://${GHTOKEN}@github.com/nrxszvo/mimicChess.git
 	cd mimicChess
 	if [ ! -e "datasets" ]; then
-		ln -s ~/mimicChessData/datasets .
+		ln -s ${HOME}/mimicChessData/datasets .
 	fi
 	if [ -z ${MYNAME+x} ]; then
 		echo "git name and email not specified; skipping git config"
@@ -37,12 +38,12 @@ if [ ! -d "/home/ubuntu/git/mimicChess" ]; then
 		git config --global user.name ${MYNAME} 
 		git config --global user.email ${MYEMAIL} 
 	fi
-	cd /home/ubuntu
+	cd ${HOME} 
 fi
 
 cd git/mimicChess
-conda env create -f environment.yml
-cd ~
+conda env update -f environment.yml
+cd ${HOME}
 
 if ! command -v npm 2>&1 >/dev/null
 then
@@ -55,21 +56,21 @@ then
 	sudo npm install --global yarn
 fi
 
-if [ ! -e "~/.vim/autoload/plug.vim" ]; then
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -e "${HOME}/.vim/autoload/plug.vim" ]; then
+	curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	sudo add-apt-repository -y ppa:jonathonf/vim
 	sudo apt update
 	sudo apt install -y vim
 fi
 
-if [ ! -d "/home/ubuntu/git/vimrc" ]; then
-	cd /home/ubuntu/git
+if [ ! -d "${HOME}/git/vimrc" ]; then
+	cd ${HOME}/git
 	git clone https://github.com/nrxszvo/vimrc.git
-	cp vimrc/vimrc ~/.vimrc
-	cp vimrc/coc-settings.json ~/.vim/coc-settings.json
+	cp vimrc/vimrc ${HOME}/.vimrc
+	cp vimrc/coc-settings.json ${HOME}/.vim/coc-settings.json
 fi
 
-echo "set -g mouse on" > ~/.tmux.conf
+echo "set -g mouse on" > ${HOME}/.tmux.conf
 
-cd ~/git/mimicChess
+cd ${HOME}/git/mimicChess
 git remote set-url origin https://nrxsvzo:${GHTOKEN}@github.com/nrxszvo/mimicChess.git
